cf8fc5f27b83d495f931b11ea66becd2328a112e
YanaKruchinina <94057217+YanaKruchinina@users.noreply.github.com>
2021-11-13 18:36:31 +0500

    Update 1.R

--- 1.R
+++ 1.R
@@ -1,9 +1,11 @@
 #загружаем данные и используем функцию ts временной ряд
 data1 <- ts(read.csv("RU_Electricity_Market_EUS_dayahead_price.csv", header = T, sep = ",")$V3,
 + start=c(2017,01), frequency=12)
+
 #Общее количество цен на каждый месяц отчетного периода составило
 cum.data1 <- ts(cumsum(data1),
 + start=c(2017,01), frequency=12)
+
 #Отобразим данные цен по месяцам и изменения цен графически.
 oldpar <- par(mfrow=c(2,1))
 plot(data1, type="b", log="y", xlab="")
